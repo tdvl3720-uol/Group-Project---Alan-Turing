9be32ba327e53855a64cdf5a31ceec4475232167
LIza <liza@LIzas-MacBook-Air.local>
2025-05-13 16:31:12 +0100

    Updated extract_answers_sequence function

--- scripts/data_extraction_M1.py
+++ scripts/data_extraction_M1.py
@@ -4,9 +4,6 @@ def extract_answers_sequence(file_path):
 
     Input: path (str) to a quiz answers text file.
 
-    Parameters:
-        file_path (str): The path to the respondent's text file.
-
     Output: List of 100 integers, where each number is 1, 2, 3, 4 (corresponding to the
     answer marked by the respondent), or 0 (if the question was not answered).
 
@@ -14,8 +11,7 @@ def extract_answers_sequence(file_path):
 
     answers = []
     with open(file_path, "r") as file:
-        lines = [line.strip() for line in file.readlines() if line.strip() != ""]
-
+        lines = [line.strip() for line in file.readlines() if line.strip() != ""] # Removing empty lines
     for i in range(0, len(lines), 5):
         question_block = lines[i:i+5]
         selected = 0
@@ -26,13 +22,18 @@ def extract_answers_sequence(file_path):
 
     return answers
 
-
 def write_answers_sequence(answers, respondent_id):
     """
+    Save the extracted sequence for further use.
+
     Input: List of 100 integers (answer sequence) and an integer n (respondent ID).
+
     Output: Writes the sequence to a text file named answers_list_respondent_n.txt.
-    Purpose: Save the extracted sequence for further use.
+
     """
-    file_name = f"answers_list_respondent_{respondent_id}.txt"
     with open(file_name, "w") as file:
-        file.write(", ".join(map(str, answers)))
+        for i in range(len(answers)):
+            file.write(str(answers[i]))
+            if i < len(answers) - 1:
+                file.write(", ")
+
